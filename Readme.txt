35326b941a8a66d9d835c82611a11aa67fa78590
Mushymato <michelle.xia@mail.utoronto.ca>
2017-03-13 23:05:01 -0400

    Settings alpha

--- Readme.txt
+++ Readme.txt
@@ -2,4 +2,9 @@ Workblocs is a Google Chrome extension.
 It replaces chrome://newtab with a simple note taking tool.
 Users can have many notes on the screen, each with a title and body.
 Notes are stored and synced across the user's google account such that the notes are retained when the user logs into chrome on another device.
-They can be downloaded as plain text files at anytime by hotkey ctrl+s;
+They can be downloaded as plain text files at anytime by hotkey ctrl+s
+
+Future Features
+	Settings (font, font-size)
+	Full hotkey support
+	
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var night = true;
 
 function initBloc(key){
 	var bloc = document.getElementById(key);
-		
+
 	var blocTitle = bloc.getElementsByClassName('title')[0];
 		blocTitle.addEventListener('input', saveTitle, false);
 		blocTitle.addEventListener('focus', changeFocus, false);
@@ -15,17 +15,17 @@ function initBloc(key){
 		blocTxt.addEventListener('input', saveTxt, false);
 		blocTxt.addEventListener('focus', changeFocus, false);
 		blocTxt.value = blocs[key][1];
-	
+
 	var blocX = bloc.getElementsByClassName('closeBloc')[0];
 		blocX.addEventListener('click', deleteBloc, false)
 }
- 
+
 function newBloc(){
 	var count = 0;
 	while(order.includes(count.toString())){
 		count += 1;
 	}
-	
+
 	var newId = count.toString();
 	blocs[newId] = ['', ''];
 
@@ -34,13 +34,13 @@ function newBloc(){
 	} else {
 		order.unshift(newId);
 	}
-	
+
 	addBloc(newId, this.id);
 	if (document.getElementsByClassName('focus').length == 0){
 		focused = document.getElementById(newId);
 		focused.className += ' focus';
 	}
-	chrome.storage.sync.set({'order': order, 'blocs': blocs, 'focus': focused.id});	
+	chrome.storage.sync.set({'order': order, 'blocs': blocs, 'focus': focused.id});
 	return false;
 }
 function addBloc(newId, direction = 'right'){
@@ -49,12 +49,12 @@ function addBloc(newId, direction = 'right'){
 			newTitle.className += 'title';
 			newTitle.setAttribute('placeholder', 'Title');
 			newTitle.setAttribute('spellcheck', 'false');
-			
+
 		var	newTxt = document.createElement('textarea');
 			newTxt.className += 'txt';
 			newTxt.setAttribute('placeholder', 'Write here');
 			newTxt.setAttribute('spellcheck', 'false');
-		
+
 		var newX = document.createElement('a');
 			newX.className += 'closeBloc';
 			newX.setAttribute('href', '#');
@@ -66,16 +66,16 @@ function addBloc(newId, direction = 'right'){
 			newDiv.appendChild(newX);
 			newDiv.appendChild(newTitle);
 			newDiv.appendChild(newTxt);
-		
+
 		var blocWrapper = document.getElementById('blocWrapper');
-		
+
 		if (direction == "right"){
 			blocWrapper.appendChild(newDiv);
 		} else {
 			blocWrapper.insertBefore(newDiv, blocWrapper.firstChild);
 		}
 	}
-	initBloc(newId);	
+	initBloc(newId);
 	return false;
 };
 
@@ -89,7 +89,7 @@ function deleteBloc(){
 			break;
 		};
 	};
-	
+
 	delete blocs[delBloc.id];
 	for(i=0; i<order.length; i++){
 		if (order[i] == delBloc.id){
@@ -97,7 +97,7 @@ function deleteBloc(){
 		}
 	}
 	chrome.storage.sync.set({'blocs':blocs, 'order':order})
-	
+
 	blocWrapper.removeChild(delBloc);
 	if (delBloc.className.match( /(?:^|\s)focus(?!\S)/g , '' )){
 		try{
@@ -108,8 +108,8 @@ function deleteBloc(){
 			newBloc();
 		}
 	}
-	
-	
+
+
 	//setTimeout(function(){
 		//document.getElementById('blocWrapper').style.opacity = 1;
 	//}, 100);
@@ -130,26 +130,25 @@ function changeFocus(){
 		return false;
 	}
 	var blocList = document.getElementsByClassName('bloc');
-	
+
 	for (i=0; i < blocList.length; i++){
 		if(blocList[i].className.match( /(?:^|\s)focus(?!\S)/g , '' )){
 			blocList[i].className = blocList[i].className.replace( /(?:^|\s)focus(?!\S)/g , '' );
 		};
 	};
-	
+
 	this.parentElement.className += ' focus';
 	focused = this.parentElement;
 	chrome.storage.sync.set({'focus':focused.id});
 	return false;
 };
-function settings(){
+function clearStorage(){
 	chrome.storage.sync.clear();
 }
 
-function downloadBlocs() {
-	//credit to thiscouldbebetter.wordpress.com
-
-    var textToSave = "";
+function writeBlocs() {
+	//put bloc data into a file and return the Blob
+	var textToSave = "";
 	for(i = 0; i < order.length; i++){
 		textToSave += "[";
 		textToSave += blocs[order[i]][0].replace(/\n\r?/g, '\r\n');
@@ -157,12 +156,18 @@ function downloadBlocs() {
 		textToSave += blocs[order[i]][1].replace(/\n\r?/g, '\r\n');
 		textToSave += "\n";
 	}
-	
-    var textToSaveAsBlob = new Blob([textToSave], {type:"text/plain"});
+	return new Blob([textToSave], {type:"text/plain"});
+}
+
+function downloadBlocs() {
+	//credit to thiscouldbebetter.wordpress.com
+
+	var textToSaveAsBlob = writeBlocs();
     var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
-    
+	alert(textToSaveAsURL);
+
 	var fileName = "Workblocs-" + new Date()+".txt";
- 
+
     var downloadLink = document.createElement("a");
     downloadLink.download = fileName;
     downloadLink.innerHTML = "Download File";
@@ -170,10 +175,15 @@ function downloadBlocs() {
     downloadLink.onclick = function(e){document.body.removeChild(e.target);};
     downloadLink.style.display = "none";
     document.body.appendChild(downloadLink);
- 
+
     downloadLink.click();
 }
 
+function saveToDrive() {
+	/*var gdrive = document.getElementsByID("gdrive");
+	gdrive.data-src=textToSaveAsURL;
+	data-filename=*/
+}
 //clock
 function startTime() {
     var today = new Date();
@@ -197,8 +207,17 @@ function adjTime(i) {
     if (i < 10) {i = "0" + i};
     return i;
 }
-//styleSheets
-function nightly(){
+
+//settings
+function settings(){
+	var sett = document.getElementById('settings');
+	if(sett.style.display == "none"){
+		sett.style.display = "block";
+	}else{
+		sett.style.display = "none";
+	}
+}
+function nightly(){ //night/day mode
 	if(!night){
 		document.styleSheets[1].disabled = true;
 		night = true;
@@ -209,15 +228,14 @@ function nightly(){
 	chrome.storage.sync.set({'night': night});
 }
 
-
 (function(window, document, undefined){
-	//chrome.storage.sync.clear();	
+	//chrome.storage.sync.clear();
 
 	window.onload = init;
-	
+
 	function init(){
 		startTime();
-		
+
 		// Capture Ctrl+S
 		var isCtrl = false;
 		document.onkeyup=function(e) {
@@ -230,18 +248,20 @@ function nightly(){
 				 return false;
 			}
 		};
-		
+
 		document.getElementById('left').addEventListener('click', newBloc, false);
 		document.getElementById('right').addEventListener('click', newBloc, false);
 		document.getElementById('clock').addEventListener('click', nightly, false);
-				
+		//document.getElementById('clock').addEventListener('contextmenu', settings, false);
+		//document.getElementById('closeSet').addEventListener('click', settings, false);
+
 		chrome.storage.sync.get(['order','blocs', 'focus', 'night'], function(item){
 			//get night sheet
 			if(item['night'] != undefined){
 				night = !item['night'];
 			}
 			nightly();
-			
+
 			//load blocs
 			if(item['blocs'] != undefined) {blocs = item['blocs'];}
 			if(item['order'] != undefined) {order = item['order'];}
@@ -255,7 +275,7 @@ function nightly(){
 				focused = document.getElementsByClassName('bloc')[0];
 			}
 			focused.className += ' focus';
-			
+
 			//overide tab
 			var textareas = document.getElementsByTagName('textarea');
 			var count = textareas.length;
@@ -265,11 +285,11 @@ function nightly(){
 						e.preventDefault();
 						var s = this.selectionStart;
 						this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
-						this.selectionEnd = s+1; 
+						this.selectionEnd = s+1;
 					}
 				}
 			}
-			
+
 			document.getElementById('blocWrapper').style.opacity = 1;
 
 			return false;
